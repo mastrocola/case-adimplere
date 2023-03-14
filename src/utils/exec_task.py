59f088a2060aee0acd2209f5d32450e0bf76ccec
Marcio Mastrocola Alcantara <mastrocola@gmail.com>
2023-03-13 23:52:37 -0300

    refactor: exec task log improvements

--- src/utils/exec_task.py
+++ src/utils/exec_task.py
@@ -3,15 +3,20 @@ from src.services.logger import Log
 from src.settings import PROJECT
 
 
+def end_time_ms(start_time):
+    return (time() - start_time) * 1000
+
+
 async def exec_task(task_function, params=None):
     start_time = time()
+    code = f'{PROJECT}_{task_function.__name__.upper()}'
+    module = task_function.__module__
+
     try:
-        exec_result = await task_function() if params is None else await task_function(params)
-
-        Log().info(f'{PROJECT}_{task_function.__name__.upper()}_FINISH', f'Finished {task_function.__module__} table generation. Elapsed time: {time() - start_time}')
-
-        return exec_result
+        return await task_function() if params is None else await task_function(params)
     except BaseException as error:  # pylint: disable=broad-except
-        Log().error(f'{PROJECT}_{task_function.__name__.upper()}_ERROR', f'Error on generate {task_function.__module__}', error)
+        Log().error(f'{code}_ERROR', f'Error in module {module}', error)
 
         return None
+    finally:
+        Log().info(f'{code}_FINISH', f'Finished {module} execution. Elapsed time: {end_time_ms(start_time)}ms')
